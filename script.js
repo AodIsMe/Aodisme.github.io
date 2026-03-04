b112c0273487ba6c5ed1ca68da6e15718fb2b546
AodIsMe <garrettbrown6877@gmail.com>
2026-03-04 00:41:59 -0500

    Update script.js
    Feat: added functions for settings button and change theme button

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//elements
+const html = document.documentElement;
+const toggleButton = document.getElementById("themeToggle");
+const settingsIcon = document.getElementById("settings-icon");
+const settingsMenu = document.getElementById("settings-menu");
+
+//navbar collapse into menu logic
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -6,3 +13,44 @@ menuIcon.onclick = () => {
   navbar.classList.toggle('active');
 }
 
+
+
+// adding theme toggle
+toggleButton.addEventListener("click", (e) => {
+  e.stopPropagation();
+
+  if (html.dataset.theme === "light") {
+    html.removeAttribute("data-theme");
+    localStorage.setItem("theme", "dark");
+  } else {
+    html.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+
+  settingsMenu.hidden= true;
+});
+
+//adding load local save on refresh
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "light") {
+  html.setAttribute("data-theme", "light");
+}
+
+//-------settings menu open/close -------
+settingsIcon.addEventListener("click", (e) => {
+  e.stopPropagation();
+  settingsMenu.hidden = !settingsMenu.hidden;
+});
+
+// close menu when clicking outside
+document.addEventListener("click", (e) => {
+  if (!e.target.closest(".settings")) {
+    settingsMenu.hidden = true;
+  }
+});
+
+// close menu on escape
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") settingsMenu.hidden = true;
+});
